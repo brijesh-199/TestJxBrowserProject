dc9199d89a19202bdd9514f4812e32e42cb54a23
brijesh-199 <brijeshpandey.k@gmail.com>
2015-11-15 13:41:44 +0530

    Added Selenium project-Jenkins -Demo

---
+++ src/Jenkins/JenkinsDemoJob.java
@@ -0,0 +1,20 @@
+package Jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class JenkinsDemoJob {
+	
+	@Test
+	public void Jenkinstest(){
+		System.out.println("Welcome to Jenkins World");
+		
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://www.facebook.com");
+		System.out.println(driver.getTitle());
+		driver.quit();
+			
+	}
+
+}
