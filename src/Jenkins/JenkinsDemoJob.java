35719ad99c819a91b2aeab81e9e884247159a5b3
brijesh-199 <brijeshpandey.k@gmail.com>
2015-11-18 21:54:32 +0530

    Adding For

--- src/Jenkins/JenkinsDemoJob.java
+++ src/Jenkins/JenkinsDemoJob.java
@@ -8,7 +8,7 @@ public class JenkinsDemoJob {
 	
 	@Test
 	public void Jenkinstest(){
-		System.out.println("Welcome to Test Git Jenkins World");
+		System.out.println("Welcome to Test Git  for Jenkins World");
 		
 		WebDriver driver=new FirefoxDriver();
 		driver.get("http://www.facebook.com");
